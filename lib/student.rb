773753d27c79a6d64e50ce045882d247e7738590
Javante Nelson <javantenelson@gmail.com>
2020-04-22 00:20:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   attr_accessor :name, :grade, :id
   #attr_reader :id 
   
-  def initialize(name,grade,id=nil)
+  def initialize(name,grade)
     @name = name 
     @grade = grade 
     @id = id 
